ef8327deb1c2e169e9aba3cfb5808a869a2e96a1
Michael Giglia <michael.a.giglia@gmail.com>
2022-11-21 16:59:04 -0500

    first edit of async log, no csv yet

--- util/cf_interface.py
+++ util/cf_interface.py
@@ -5,10 +5,13 @@ import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.utils.callbacks import Caller
 from cflib.utils import uri_helper
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.log import LogConfig
 
 from threading import Thread
 import yaml
 from util.dict_search import nested_search
+from util.data_struct import DataStruct
 
 class CommandValues(dict):
     def __init__(self, r=0.0, p=0.0, ydot=0.0, thrust=0):
@@ -30,6 +33,8 @@ class CFBase:
 class CFDroneControls(CFBase):
     def __init__(self, params_file):
         self.connected = False
+        self.step_test_completed = False
+        self.step_test_data = DataStruct()
         try:
             if type(params_file) is str:
                 self.params_dict = yaml.safe_load(open(params_file, 'r'))
@@ -43,12 +48,41 @@ class CFDroneControls(CFBase):
             self.initialized = False
         self.address = self.params_dict['cf']['address']
         self.connect()
+    
+    def async_log(self, scf, logconf):
+        cf = scf.cf
+        cf.log.add_config(logconf)
+        logconf.data_received_cb.add_callback(self.log_callback)
+        logconf.start() #TODO: need to move this somewhere else
+        # time.sleep(5) #TODO: REMOVE
+        # logconf.stop() #TODO: Need to move this somewhere else as well
+        while self.step_test_completed is False:
+            time.sleep(0.5)
+        logconf.stop()
+
+    def log_callback(self, timestamp, data, logconf):
+        # TODO: Make this store data into an array, then push to CSV
+        self.step_test_data.timestamps.append(timestamp)
+        self.step_test_data.data.append(data)
+        # print('[%d][%s]: %s' % (timestamp, logconf.name, data))
 
     def connect(self):
         try:
             uri = uri_helper.uri_from_env(default=self.address)
             cflib.crtp.init_drivers(enable_debug_driver=False)
             self.cf = Crazyflie(rw_cache='./cache')
+
+            self.lg_stab = LogConfig(name='Stabilizer', period_in_ms=50)
+            self.lg_stab.add_variable('stabilizer.roll', 'float')
+            self.lg_stab.add_variable('stabilizer.pitch', 'float')
+            self.lg_stab.add_variable('stabilizer.yaw', 'float')
+            self.lg_stab.add_variable('stateEstimateZ.rateRoll', 'float')
+            self.lg_stab.add_variable('stateEstimateZ.ratePitch', 'float')
+            self.lg_stab.add_variable('stateEstimateZ.rateYaw', 'float')
+
+            self.scf = SyncCrazyflie(uri, cf=self.cf)
+            self.log_thread = Thread(target=self.async_log, args=(self.scf, self.lg_stab))
+
             self.cf.open_link(uri)
             print(f"Sucessfully connected to {self.address}")
             self.connected = True
@@ -99,6 +133,9 @@ class CFDroneControls(CFBase):
         yaml.dump(self.pulled_params, open(filename, 'w'))
     
     def basic_step_test(self):
+
+        self.step_test_completed = False
+        self.log_thread.start()
         cmd0 = CommandValues()
         cmd1 = CommandValues(r=0, p=0, ydot=0, thrust=20000)
         cmd2 = CommandValues(r=0, p=30, ydot=0, thrust=20000)
@@ -120,6 +157,9 @@ class CFDroneControls(CFBase):
             self.send_command(cmd1)
             time.sleep(0.1)
         self.send_command(cmd0)
-        
+
+        self.step_test_completed = True
+        print("DONE")
+
     def send_command(self, cmd: CommandValues):
         self.cf.commander.send_setpoint(cmd['roll'], cmd['pitch'], cmd['yawdot'], cmd['thrust'])
---
+++ util/data_struct.py
@@ -0,0 +1,20 @@
+import csv
+import numpy
+# import matplotlib.pyplot as plt
+
+class DataStruct:
+    def __init__(self, ):
+        self.timestamps = []
+        self.data = []
+
+    def to_csv(self):
+        pass
+        #TODO: Export to csv, self.data is a dictionary with group.name format (stabilizer.yaw) for example
+
+    def from_csv(self):
+        pass
+        #TODO: MAYBE NOT YET
+
+    def basic_plot(self):
+        pass
+
