3f3f9c904e91d93b97ccf4b6324e4fce09823908
Michael Giglia <michael.a.giglia@gmail.com>
2022-11-21 16:32:31 -0500

    basic step test done

--- cf_test.py
+++ cf_test.py
@@ -1,9 +1,13 @@
-from util.cf_interface import CFDroneControls
+from util.cf_interface import CFDroneControls, CommandValues
+
 
 if __name__ == "__main__":
     cfdc = CFDroneControls('yamls/cf_test_1.yaml')
     # cfdc.get_params()
     # cfdc.dump_yaml()
+    i = CommandValues(0,0,0,40000)
+    f = CommandValues(0,30,0,40000)
     cfdc.set_params()
     cfdc.basic_step_test()
+    # cfdc.basic_step_test(init_command=i, step_command=f, step_time=5)
     cfdc.disconnect()
